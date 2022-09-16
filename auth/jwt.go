3849062f196f1d6c30a76903300ba44cf3049528
Lukas Grimm <lukaskingsmail@gmail.com>
2022-09-16 17:46:52 +0200

    Implemented Header decoder

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,5 +1,13 @@
 package auth
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Jwt string
 
 type header struct {
@@ -9,11 +17,39 @@ type header struct {
 
 type payload map[string]interface{}
 
-func (jwt *Jwt) Header() header {
-	return header{
-		Typ: "JWT",
-		Alg: "HS256",
+func (jwt *Jwt) Header() (header, error) {
+	splitJwt := strings.SplitN(string(*jwt), ".", 3)
+	headerMap, err := readBase64Json(splitJwt[0])
+	if err != nil {
+		return header{}, err
 	}
+
+	alg, algOk := headerMap["alg"].(string)
+	typ, typOk := headerMap["typ"].(string)
+	if !algOk || !typOk {
+		return header{}, errors.New("invalid header")
+	}
+
+	return header{
+		Alg: alg,
+		Typ: typ,
+	}, nil
+}
+
+func readBase64Json(base64Json string) (map[string]interface{}, error) {
+	utf8Json, err := base64.StdEncoding.DecodeString(base64Json)
+	if err != nil {
+		return map[string]interface{}{}, errors.New("cannot convert from base64")
+	}
+
+	var object map[string]interface{}
+	err = json.Unmarshal(utf8Json, &object)
+	fmt.Println(object)
+	if err != nil {
+		return map[string]interface{}{}, errors.New("cannot unmarschal json")
+	}
+
+	return object, nil
 }
 
 func (jwt *Jwt) Payload() payload {
@@ -25,5 +61,5 @@ func (jwt *Jwt) Validate(key string) error {
 }
 
 func CreateJwt() Jwt {
-	return ".."
+	return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.."
 }
--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -15,11 +15,55 @@ type JwtTestSuite struct {
 func (suite *JwtTestSuite) SetupTest() {
 }
 
+func (suite *JwtTestSuite) TestJwt_HeaderHasHs256Alg() {
+	token := Jwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..")
+
+	header, err := token.Header()
+	assert.Nil(suite.T(), err)
+	assert.NotNil(suite.T(), header)
+	assert.Equal(suite.T(), "HS256", header.Alg)
+}
+
+func (suite *JwtTestSuite) TestJwt_HeaderHasRs256Alg() {
+	token := Jwt("ewogICJhbGciOiAiUlMyNTYiLAogICJ0eXAiOiAiSldUIgp9..")
+
+	header, err := token.Header()
+	assert.Nil(suite.T(), err)
+	assert.NotNil(suite.T(), header)
+	assert.Equal(suite.T(), "RS256", header.Alg)
+}
+
+func (suite *JwtTestSuite) TestJwt_ErrorWhenNonBase64Encoded() {
+	invalidBase64Token := Jwt("ewogICJhbGciOiAiUlMyNTYiL%AogICJ0eXAiOiAiSldUIgp9..")
+
+	header, err := invalidBase64Token.Header()
+	assert.ErrorContains(suite.T(), err, "cannot convert from base64")
+	assert.NotNil(suite.T(), header)
+}
+
+func (suite *JwtTestSuite) TestJwt_ErrorWhenInvalidJsonEncoded() {
+	invalidJsonToken := Jwt("ewogICJhbGciOiAiUlMyNTYiLAogICJ0eXAiOiAiSldUCn0=..")
+
+	header, err := invalidJsonToken.Header()
+	assert.ErrorContains(suite.T(), err, "cannot unmarschal json")
+	assert.NotNil(suite.T(), header)
+}
+
+func (suite *JwtTestSuite) TestJwt_ErrorWhenHeaderFieldMissing() {
+	invalidJsonToken := Jwt("ewogICJhbGciOiAiUlMyNTYiLAogICJ0eXBlIjogIkpXVCIKfQ==..")
+
+	header, err := invalidJsonToken.Header()
+	assert.ErrorContains(suite.T(), err, "invalid header")
+	assert.NotNil(suite.T(), header)
+}
+
 func (suite *JwtTestSuite) TestCreateJwt() {
 	token := CreateJwt()
 
-	assert.Equal(suite.T(), "HS256", token.Header().Alg)
-	assert.Equal(suite.T(), "JWT", token.Header().Typ)
+	header, err := token.Header()
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), "HS256", header.Alg)
+	assert.Equal(suite.T(), "JWT", header.Typ)
 	assert.Equal(suite.T(), make(map[string]interface{}), map[string]interface{}(token.Payload()))
 	assert.Nil(suite.T(), token.Validate("key"))
 }
